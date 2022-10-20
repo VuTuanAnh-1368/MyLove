7f1156e1278f0b50d1f46b6b4c5d0d0d1d79c18c
VuTuanAnh-1368 <92041804+VuTuanAnh-1368@users.noreply.github.com>
2022-10-20 23:54:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello Bạn iu của mình!",
+  introDesc: `Nhân ngày 20/10 chúc bạn iu\n
+    ngày càng xinh gái\n
+    học giỏi, đáng yêu :333\n
+    ...Yêu thương mình nhiều hơn nữa nhé`,
+  btnIntro: "😘😘😘",
+  title: "Công túa của tôi Trần Phương Thảo 😙",
+  desc: "Thích bé đáng yêu như nàyyyy     . Chọn ik nàyy ",
+  btnYes: "Em iu anh nhất trên đời :33",
+  btnNo: "Không -,- ",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh :33",
+  btnReply: "Gửi cho anh 😘",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Iuu bé rất nhiều ❣️❣️❣️",
+  messDesc: "Đi cùng anh đến sau này nhóoo béiuuu ♥️",
+  btnAccept: "Vâng anh iu 🤣🤣🤣 ",
+  messLink: "https://www.facebook.com/messages/t/100010975544524", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
